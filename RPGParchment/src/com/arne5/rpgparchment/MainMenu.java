f6fec0a74214e2f7c3be3f62e2bc4fa16c344191
Dave Arneson <urvaius@gmail.com>
2014-02-09 21:59:38 -0800

    started a menu

---
+++ RPGParchment/src/com/arne5/rpgparchment/MainMenu.java
@@ -0,0 +1,15 @@
+package com.arne5.rpgparchment;
+
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainMenu extends Activity implements OnClickListener{
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
