bddd6405a1ea984bda447db54fede7a61c33fe1a
Dave Arneson <urvaius@gmail.com>
2014-02-11 00:28:58 -0800

    basic splash page started

--- RPGParchment/src/com/arne5/rpgparchment/MainActivity.java
+++ RPGParchment/src/com/arne5/rpgparchment/MainActivity.java
@@ -1,23 +1,62 @@
 package com.arne5.rpgparchment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	MediaPlayer themeSong;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		Thread timer = new Thread() {
+			public void run() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+				try {
+					sleep(20000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} finally {
+					Intent mainmenu = new Intent(
+							"com.arne5.rpgparchment.MainMenu");
+					startActivity(mainmenu);
+				}
+			}
+
+		};
+		timer.start();
+
+	}
+
+	@Override
+	protected void onResume() {
+
+		themeSong = MediaPlayer.create(this, R.raw.itcrowdtheme);
+		themeSong.start();
+
+		super.onResume();
+
+	}
+
+	@Override
+	protected void onPause() {
+
+		themeSong.stop();
+		themeSong.release();
+
+		super.onPause();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
--- RPGParchment/src/com/arne5/rpgparchment/MainMenu.java
+++ RPGParchment/src/com/arne5/rpgparchment/MainMenu.java
@@ -1,10 +1,20 @@
 package com.arne5.rpgparchment;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 public class MainMenu extends Activity implements OnClickListener{
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_menu);
+	}
 
 	@Override
 	public void onClick(View arg0) {
